073f1ea61e21ca221fd512169bf498b4739bb479
Pure Krome <github@world-domination.com.au>
2025-10-21 00:19:08 +1100

    📦 NuGet package updates

--- src/SimpleTestcontainers.xUnit.v3.PostgreSQL/PostgreSqlFixture.cs
+++ src/SimpleTestcontainers.xUnit.v3.PostgreSQL/PostgreSqlFixture.cs
@@ -7,7 +7,7 @@ namespace WorldDomination.SimpleTestcontainers.xUnit.v3.PostgreSQL;
 // <summary>
 /// Initializes a new instance of the <see cref="SqlServerFixture"/> class with some default settings.
 ///   - Container Name: SqlServer-Tests
-///   - Image: mcr.microsoft.com/mssql/server:2022-latest
+///   - Image: https://hub.docker.com/layers/library/postgres/15.1/images/sha256-f25135c3038e660f35edd6bc752769b67fa0c43dec1f691069f8089f6c2fd7a0
 /// </summary>
 public sealed class PostgreSqlFixture : IDatabaseFixture, IAsyncLifetime
 {
